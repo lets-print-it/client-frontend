e84357f6217f3f03ffd39ca0c04fb277eeed10a4
Evgeny Evseev <evgeny@evseevs.ru>
2024-03-19 02:32:29 +0300

    revert to react 18, fix conflicts

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from "react";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { render } from "react-dom";
 
-const rootElement = document.getElementById("root");
-render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  rootElement,
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  // <React.StrictMode>
+  <App />,
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
